ac0fd8b023f8c06b107cd3c9d9ffb6cb30c03fbc
Yash Godara <yash17godara@gmail.com>
2023-02-22 18:23:57 +0530

    fixed bugs
     for mobile view , responsive

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 //Toggle Navbar Search Bar
 //Toggle Navbar Cart Bar
 //Toggle Navbar Login Bar
+//On Window Scroll Navbar Icon hide
 
 
 
@@ -19,12 +20,14 @@ for (let i = 0; i < btns.length; i++) {
     });
 }
 
-
 //Toggle Menu Bar for Mobiles
 let menuForm = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     menuForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 
@@ -33,6 +36,9 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    menuForm.classList.remove('active');
+    cartForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 
@@ -41,6 +47,9 @@ let cartForm = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () => {
     cartForm.classList.toggle('active');
+    menuForm.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 //Toggle Login Bar
@@ -48,10 +57,18 @@ let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
+    menuForm.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartForm.classList.remove('active');
 }
 
-//
-
+//On Window Scroll Navbar Icon hide
+window.scroll = () => {
+    menuForm.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartForm.classList.remove('active');
+    loginForm.classList.remove('active');
+}
 
 
 
