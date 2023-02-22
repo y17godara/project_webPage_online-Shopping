18f53b6393b276d527c849587c3bc0df368a504c
Yash Godara <yash17godara@gmail.com>
2023-02-22 06:42:19 +0530

    update
    files added

---
+++ js/script.js
@@ -0,0 +1,58 @@
+//Content
+//Active Navbar
+//Toggle Navbar Search Bar
+//Toggle Navbar Cart Bar
+//Toggle Navbar Login Bar
+
+// Add active class to the current button (highlight it)
+let header = document.getElementById("yashDiv");
+let btns = header.getElementsByClassName("yashBtn");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+//Toggle Search Bar
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+
+//Toggle Cart Bar
+let cartForm = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartForm.classList.toggle('active');
+}
+
+//Toggle Login Bar
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+    loginForm.classList.toggle('active');
+}
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
